c2973000ae024ebe6af636aaa61d57fa6d6c6d96
andrew-kozubenko <a.kozubenko@g.nsu.ru>
2026-02-04 15:36:31 +0700

    init commit
    Spring initiallization

---
+++ GoldenPunchTeam/src/main/java/golden/punch/team/GoldenPunchTeam/GoldenPunchTeamApplication.java
@@ -0,0 +1,13 @@
+package golden.punch.team.GoldenPunchTeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenPunchTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenPunchTeamApplication.class, args);
+	}
+
+}
---
+++ GoldenPunchTeam/src/test/java/golden/punch/team/GoldenPunchTeam/GoldenPunchTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package golden.punch.team.GoldenPunchTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenPunchTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
